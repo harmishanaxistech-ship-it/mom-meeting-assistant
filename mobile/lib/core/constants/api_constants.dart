ee7e5f8c7e016a143cb14bf7593274eda63fe10f
Harmish Sejpal <harmish.anaxistech@gmail.com>
2026-09-03 14:08:57 +0530

    chore: update cloudflare tunnel base url

--- mobile/lib/core/constants/api_constants.dart
+++ mobile/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Ultra-Fast Cloudflare Enterprise HTTPS Tunnel (Zero USB cable, works on all Wi-Fi & 4G/5G networks)
-  static const String serverBaseUrl = 'https://romance-letter-insight-airport.trycloudflare.com';
+  static const String serverBaseUrl = 'https://acts-prince-rate-protein.trycloudflare.com';
   static const String baseUrl = '$serverBaseUrl/api';
 
   static const String login = '/auth/login';
