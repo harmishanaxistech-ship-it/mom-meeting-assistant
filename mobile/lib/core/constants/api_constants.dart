367d5704257cfd31c24583d000cf3fb8cdf6b109
Harmish Sejpal <harmish.anaxistech@gmail.com>
2026-09-10 18:42:42 +0530

    8010: chore: Switch mobile app back to live NoteAX server URL

--- mobile/lib/core/constants/api_constants.dart
+++ mobile/lib/core/constants/api_constants.dart
@@ -1,8 +1,8 @@
 class ApiConstants {
   // Local USB Testing via adb reverse:
-  static const String serverBaseUrl = 'http://127.0.0.1:5001';
+  // static const String serverBaseUrl = 'http://127.0.0.1:5001';
   // Live Server URL:
-  // static const String serverBaseUrl = 'https://noteaxapi.anaxistech.com';
+  static const String serverBaseUrl = 'https://noteaxapi.anaxistech.com';
   // static const String serverBaseUrl = 'http://10.0.2.2:5001'; // Android Emulator
 
   static const String baseUrl = '$serverBaseUrl/api';
